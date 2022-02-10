52bb4c54352e68be39ed6ef007fa043f7eca87c2
kush1912 <vidyajaykushwaha@gmail.com>
2022-02-11 00:21:25 +0530

    event delegation

--- Frontend/Events Propogation/Events.js
+++ Frontend/Events Propogation/Events.js
@@ -3,7 +3,8 @@
 
 // addEventListener(eventNam, CallBackFunction, true/false)
 //true -  capture, false - bubble(default)
-// Event Delegation
+// Event Delegation: Instead of adding event handlers to every html component we should attach it to the parent of
+//  the html components, by using the conecpt of event bubbling. 
 
 
 window.onload=function(){
@@ -17,5 +18,20 @@ window.onload=function(){
     
     document.querySelector('#child').addEventListener('click', ()=>{
         console.log('Child!');
+    })
+
+    document.querySelector("#category").addEventListener('click',(e)=>{
+        console.log(e.target.id);
+        //the check is required cause many other tags could have same ids  
+        if(e.target.tagName=='LI'){
+            window.location.href='/'+e.target.id;
+        }
+    });
+
+    document.querySelector('#form').addEventListener('keyup',(e)=>{
+        console.log(e);
+        if(e.target.dataset.uppercase!=undefined){
+            e.target.value = e.target.value.toUpperCase();
+        }
     })
   }
